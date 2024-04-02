cc4cef31918bf7b5da25d9ed1bfb04b88ee82a67
PersianFlacc <129433476+PersianFlacc@users.noreply.github.com>
2024-04-02 13:35:45 +0300

    Add files via upload

---
+++ src/test/java/org/example/project23/SpringProject23ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.project23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProject23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
